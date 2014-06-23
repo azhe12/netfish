53f38c34d416ecdadb565c283dab0da828a2cfbc
azhe_liu <azhe_liu@htc.com>
2014-06-23 19:55:08 +0800

    [netfish] add tid to logging module

--- Logging.h
+++ Logging.h
@@ -3,12 +3,14 @@
 #ifndef NETFISH_LOGGING_H
 #define NETFISH_LOGGING_H
 #include <stdio.h>
+#include "Thread.h"
 #include "Timestamp.h"
 
 namespace netfish {
 
-#define LOG_INFO(fmt, args...) (printf(  "%s INFO " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), ##args, __FILE__, __LINE__) )
-#define LOG_DEBUG(fmt, args...) (printf(  "%s DEBUG " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), ##args, __FILE__, __LINE__) )
+#define LOG_INFO(fmt, args...) (printf(  "%s %d INFO " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), netfish::tid(),  ##args, __FILE__, __LINE__) )
+#define LOG_DEBUG(fmt, args...) (printf(  "%s %d DEBUG " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), netfish::tid(), ##args, __FILE__, __LINE__) )
+#define LOG_WARN(fmt, args...) (printf(  "%s %d WARN " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), netfish::tid(), ##args, __FILE__, __LINE__) )
 
 }
 #endif  //NETFISH_LOGGING_H
