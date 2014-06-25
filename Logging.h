344d5a6fd130c718aa7a32f89cd5ee77211ff78a
azhe_liu <azhe_liu@htc.com>
2014-06-25 14:37:22 +0800

    [netfish][Logging] add LOG_ERROR

--- Logging.h
+++ Logging.h
@@ -8,9 +8,12 @@
 
 namespace netfish {
 
-#define LOG_INFO(fmt, args...) (printf(  "%s %d INFO " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), netfish::tid(),  ##args, __FILE__, __LINE__) )
+#define LOG_TRACE(fmt, args...) (printf(  "%s %d TRACE " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), netfish::tid(),  ##args, __FILE__, __LINE__) )
 #define LOG_DEBUG(fmt, args...) (printf(  "%s %d DEBUG " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), netfish::tid(), ##args, __FILE__, __LINE__) )
+#define LOG_INFO(fmt, args...) (printf(  "%s %d INFO " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), netfish::tid(),  ##args, __FILE__, __LINE__) )
 #define LOG_WARN(fmt, args...) (printf(  "%s %d WARN " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), netfish::tid(), ##args, __FILE__, __LINE__) )
+#define LOG_ERROR(fmt, args...) (printf(  "%s %d ERROR " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), netfish::tid(), ##args, __FILE__, __LINE__) )
+#define LOG_FATAL(fmt, args...) {printf(  "%s %d FATAL " fmt " - [%s:%d]\n", netfish::Timestamp::now().toFormattedString().c_str(), netfish::tid(), ##args, __FILE__, __LINE__);abort(); }
 
 }
 #endif  //NETFISH_LOGGING_H
