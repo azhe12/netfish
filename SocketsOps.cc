6fc24229ba803f127b7d258ca81de48b7df770bd
azhe_liu <azhe_liu@htc.com>
2014-06-26 16:09:08 +0800

    [netfish][SocketsOps] some option for socket

---
+++ SocketsOps.cc
@@ -0,0 +1,205 @@
+//azhe liuyuanzhe123@126.com
+#include "SocketsOps.h"
+#include "Logging.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>  //snprintf
+#include <string.h> //bzero
+#include <sys/socket.h>
+#include <unistd.h>
+
+using namespace netfish;
+
+namespace {
+typedef struct sockaddr SA;
+//将sockaddr_in类型转换为sockaddr
+const SA* sockaddr_cast(const struct sockaddr_in* addr)
+{
+    return static_cast<const SA*>(static_cast<const void*>(addr));
+}
+
+SA* sockaddr_cast(struct sockaddr_in* addr)
+{
+    return static_cast<SA*>(static_cast<void*>(addr));
+}
+
+void setNonBlockAndCloseOnExec(int sockfd)
+{
+    int flags = ::fcntl(sockfd, F_GETFL, 0);
+    flags |= O_NONBLOCK;
+    int ret = ::fcntl(sockfd, F_SETFL, flags);
+    if (ret < 0) {
+        LOG_ERROR("set fd:%d non-block faild!", sockfd);
+        return;
+    }
+
+    flags = ::fcntl(sockfd, F_GETFD, 0);
+    flags |= FD_CLOEXEC;
+    ret = ::fcntl(sockfd, F_SETFD, flags);
+    if (ret < 0) {
+        LOG_ERROR("set fd:%d FD_CLOEXEC faild!", sockfd);
+        return;
+    }
+}
+
+}
+
+int sockets::createNonblockingOrDie()
+{
+    int sockfd = ::socket(AF_INET,
+                        SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 
+                        IPPROTO_TCP);
+    if (sockfd < 0) {
+        LOG_FATAL("sockets::createNonblockingOrDie()");
+    }
+    return sockfd;
+}
+
+int sockets::connect(int sockfd, const struct sockaddr_in& addr)
+{
+    return ::connect(sockfd, sockaddr_cast(&addr), sizeof(addr));
+}
+
+void sockets::bindOrDie(int sockfd, const struct sockaddr_in& addr)
+{
+    int ret = ::bind(sockfd, sockaddr_cast(&addr), sizeof(addr));
+    if (ret < 0) {
+        LOG_FATAL("sockets::bindOrDie()");
+    }
+}
+
+void sockets::listenOrDie(int sockfd)
+{
+    int ret = ::listen(sockfd, SOMAXCONN);
+    if (ret < 0) {
+        LOG_FATAL("sockets::listenOrDie()");
+    }
+}
+
+int sockets::accept(int sockfd, struct sockaddr_in* addr)
+{
+    socklen_t addrlen = sizeof(*addr);
+    //从listen的sockfd请求队列中取出第一个连接
+    int connfd = ::accept4(sockfd, sockaddr_cast(addr),
+                         &addrlen, SOCK_NONBLOCK | SOCK_CLOEXEC);
+    if (connfd < 0) {
+        int savedErrno = errno;
+        LOG_ERROR("sockets::accept()");
+        switch (savedErrno) {
+            case EAGAIN:
+            case ECONNABORTED:
+            case EINTR:
+            case EPROTO:
+            case EPERM:
+            case EMFILE:    //达到单个进程打开描述符的上限
+                errno = savedErrno;
+                break;
+            case EBADF:
+            case EFAULT:
+            case EINVAL:
+            case ENFILE:
+            case ENOBUFS:
+            case ENOMEM:
+            case ENOTSOCK:
+            case EOPNOTSUPP:
+            //unexcepted errors
+                LOG_FATAL("unexcepted error of sockets::accept errno: %d %s", savedErrno, strerror(savedErrno));
+                break;
+            default:
+                LOG_FATAL("unknow error of sockets::accept errno: %d %s", savedErrno, strerror(savedErrno));
+                break;
+
+        }
+    }
+    return connfd;
+}
+
+void sockets::close(int sockfd)
+{
+    if (::close(sockfd) < 0) {
+        LOG_ERROR("sockets::close");
+    }
+}
+
+void sockets::shutdownWrite(int sockfd)
+{
+    if (::shutdown(sockfd, SHUT_WR) < 0) {
+        LOG_ERROR("sockets::shutdownWrite");
+    }
+}
+
+void sockets::toHostPort(char * buf, size_t size,
+                         const struct sockaddr_in& addr)
+{
+    char host[INET_ADDRSTRLEN] = "INVALID";
+    ::inet_ntop(AF_INET, &addr.sin_addr, host, sizeof(host));
+    uint16_t port = sockets::networkToHost16(addr.sin_port);
+    snprintf(buf, size, "%s:%u", host, port);
+}
+
+void sockets::fromHostPort(const char *ip, uint16_t port,
+                           struct sockaddr_in* addr)
+{
+    addr->sin_family = AF_INET;
+    addr->sin_port = hostToNetwork16(port);
+    if (::inet_pton(AF_INET, ip, &addr->sin_addr) <= 0) {
+        LOG_ERROR("sockets::fromHostPort");
+    }
+}
+
+struct sockaddr_in sockets::getLocalAddr(int sockfd)
+{
+    struct sockaddr_in localaddr;
+    bzero(&localaddr, sizeof(localaddr));
+    socklen_t addrlen = sizeof(localaddr);
+    if (::getsockname(sockfd, sockaddr_cast(&localaddr), &addrlen) < 0) {
+        LOG_ERROR("sockets::getLocalAddr");
+    }
+    return localaddr;
+}
+
+//得到远端sockaddr, 必须在建立连接之后
+struct sockaddr_in sockets::getPeerAddr(int sockfd)
+{
+    struct sockaddr_in peeraddr;
+    bzero(&peeraddr, sizeof(peeraddr));
+    socklen_t addrlen = sizeof(peeraddr);
+    if (::getpeername(sockfd, sockaddr_cast(&peeraddr), &addrlen) < 0) {
+        LOG_ERROR("sockets::getPeerAddr");
+    }
+    return peeraddr;
+}
+
+int sockets::getSocketError(int sockfd)
+{
+    int optval;
+    socklen_t optlen = sizeof(optval);
+
+    if (::getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &optval, &optlen) < 0) {
+        return errno;
+    } else {
+        return optval;
+    }
+}
+
+bool sockets::isSelfConnect(int sockfd)
+{
+    struct sockaddr_in localaddr = getLocalAddr(sockfd);
+    struct sockaddr_in peeraddr = getPeerAddr(sockfd);
+    return localaddr.sin_port == peeraddr.sin_port && 
+           localaddr.sin_addr.s_addr == peeraddr.sin_addr.s_addr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SocketsOps.h
@@ -0,0 +1,67 @@
+//azhe liuyuanzhe123@126.com
+#ifndef NETFISH_SOCKETSOPS_H
+#define NETFISH_SOCKETSOPS_H
+#include <arpa/inet.h>
+#include <endian.h>
+
+namespace netfish {
+
+namespace sockets {
+
+inline uint64_t hostToNetwork64(uint64_t host64)
+{
+    return htobe64(host64);
+}
+
+inline uint32_t hostToNetwork32(uint32_t host32)
+{
+    return htonl(host32);
+}
+
+inline uint16_t hostToNetwork16(uint16_t host16)
+{
+    return htons(host16);
+}
+
+inline uint64_t networkToHost64(uint64_t net64)
+{
+    return be64toh(net64);
+}
+
+inline uint32_t networkToHost32(uint64_t net32)
+{
+    return ntohl(net32);
+}
+
+inline uint16_t networkToHost16(uint64_t net16)
+{
+    return ntohs(net16);
+}
+
+//create nonblocking Socket fd
+//abort if failed.
+int createNonblockingOrDie();
+
+int connect(int sockfd, const struct sockaddr_in & addr);
+void bindOrDie(int sockfd, const struct sockaddr_in & addr);
+void listenOrDie(int sockfd);
+int accept(int sockfd, struct sockaddr_in * addr);
+void close(int sockfd);
+void shutdownWrite(int sockfd);
+
+//根据addr生成形如"ip:port" 的string
+void toHostPort(char * buf, size_t size, 
+                const struct sockaddr_in & addr);
+//根据ip和port生成sockaddr_in
+void fromHostPort(const char* ip, uint16_t port,
+                  struct sockaddr_in* addr);
+
+struct sockaddr_in getLocalAddr(int sockfd);
+struct sockaddr_in getPeerAddr(int sockfd);
+
+int getSocketError(int sockfd);
+bool isSelfConnect(int sockfd);
+
+}
+}
+#endif  //NETFISH_SOCKETSOPS_H
