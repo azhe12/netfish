f20b6228d62a5c1bbb87fe4530d5682ee1c65df2
azhe_liu <liuyuanzhe123@126.com>
2014-07-04 16:12:17 +0800

    [netfish][testTcpClient] add test for TcpClient

---
+++ testTcpClient.cc
@@ -0,0 +1,48 @@
+//azhe liuyuanzhe123@126.com
+#include "EventLoop.h"
+#include "TcpClient.h"
+#include "InetAddress.h"
+#include "TcpConnection.h"
+#include "Callbacks.h"
+
+#include <stdio.h>
+
+using namespace netfish;
+
+void onConnection(const TcpConnectionPtr& conn)
+{
+    if (conn->connected()) {
+        printf("onConnection connection[%s] - from %s to %s",
+                conn->name().c_str(),
+                conn->localAddress().toHostPort().c_str(),
+                conn->peerAddress().toHostPort().c_str());
+    } else {
+        printf("onConnection connection[%s] is down",
+                conn->name().c_str());
+    }
+}
+
+void onMessage(const TcpConnectionPtr& conn,
+               Buffer* buf,
+               Timestamp time)
+{
+    printf("onMessage(): receive %lu bytes from connection[%s] at %s",
+            buf->readableBytes(),
+            conn->name().c_str(),
+            time.toFormattedString().c_str());
+
+    printf("onMessage(): [%s]", buf->retrieveAsString().c_str());
+}
+
+int main()
+{
+    EventLoop loop;
+    InetAddress serverAddr(9876);
+    TcpClient client(&loop, serverAddr);
+
+    client.setConnectionCallback(onConnection);
+    client.setMessageCallback(onMessage);
+
+    client.connect();
+    loop.loop();
+}
