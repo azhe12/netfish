e6aa790c152b9972092f190e84c66990fa09f638
azhe_liu <liuyuanzhe123@126.com>
2014-07-07 14:24:50 +0800

    [netfish][pingpong] throughput test

---
+++ multiThreadThroughput.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ 
+killall pingPongServer
+timeout=${timeout:-100}
+bufsize=16384
+ 
+for nosessions in 100 1000; do
+  for nothreads in 1 2 3 4; do
+    sleep 5
+    echo "Bufsize: $bufsize Threads: $nothreads Sessions: $nosessions"
+    ./pingPongServer 0.0.0.0 55555 $nothreads $bufsize & srvpid=$!
+    ./pingPongClient 127.0.0.1 55555 $nothreads $bufsize $nosessions $timeout
+    kill -9 $srvpid
+  done
+done
--- pingPongServer.cc
+++ pingPongServer.cc
@@ -1,93 +1,63 @@
-//azhe liuyuanzhe123@126.com
-
 #include "TcpServer.h"
+#include "Atomic.h"
+#include "Logging.h"
+#include "Thread.h"
 #include "EventLoop.h"
 #include "InetAddress.h"
-#include "Callbacks.h"
-#include "TcpConnection.h"
-#include "Thread.h"
 
-#include <string.h>
+#include <boost/bind.hpp>
+
+#include <utility>
+
 #include <stdio.h>
-#include <string>
-#include <algorithm>
+#include <unistd.h>
 
 using namespace netfish;
 
-std::string msg;
-double g_totalSize = 0;       //kBytes
-double g_sizePerInterval = 0;   //Bytes
-double g_interval = 3;        //s
-double g_totalTime = 0;
-
 void onConnection(const TcpConnectionPtr& conn)
 {
-    if (conn->connected()) {
-        printf("onConnection(): tid %d new connection [%s] from %s\n",
-                tid(),
-                conn->name().c_str(),
-                conn->peerAddress().toHostPort().c_str());
-
-        conn->send(msg);
-    } else {
-        printf("onConnection(): tid=%d connection [%s] is down\n",
-                tid(),
-                conn->name().c_str());
-    }
+  if (conn->connected())
+  {
+    conn->setTcpNoDelay(true);
+  }
 }
 
-void onMessage(const TcpConnectionPtr& conn,
-               Buffer* buf,
-               Timestamp time)
+void onMessage(const TcpConnectionPtr& conn, Buffer* buf, Timestamp)
 {
-#if 0
-    printf("onMessage(): tid=%d receive %0.2f KBytes from connection [%s] at %s\n",
-            tid(),
-            buf->readableBytes() / 1024.0,
-            conn->name().c_str(),
-            time.toFormattedString().c_str());
-#endif
-
-    g_sizePerInterval += buf->readableBytes();
-    conn->send(buf->retrieveAsString());
+  conn->send(buf->retrieveAsString());
 }
 
-void calThroughPut()
+int main(int argc, char* argv[])
 {
-    printf("calThroughPut(): ThroughPut %0.2f MB/S\n",
-            g_sizePerInterval / (g_interval * 1024 * 1024));
-    g_totalTime += g_interval;
-    g_totalSize += g_sizePerInterval;
-    g_sizePerInterval = 0;
-}
-//arg[1]: message size , KBytes
-//arg[2]: server thread num
-int main(int argc, char** argv)
-{
+  if (argc < 4)
+  {
+    fprintf(stderr, "Usage: server <address> <port> <threads>\n");
+  }
+  else
+  {
+    //LOG_INFO << "pid = " << getpid() << ", tid = " << CurrentThread::tid();
+    LOG_INFO("tid = %d", tid());
+
+    const char* ip = argv[1];
+    uint16_t port = static_cast<uint16_t>(atoi(argv[2]));
+    InetAddress listenAddr(ip, port);
+    int threadCount = atoi(argv[3]);
+
     EventLoop loop;
-    InetAddress listenAddr(9876);
+
     TcpServer server(&loop, listenAddr);
-    size_t len = 16; //kBytes
 
-    if (argc >= 2) {    //len
-        len = atoi(argv[1]);
-    }
-
-    msg.resize(len * 1024);
-    std::fill(msg.begin(), msg.end(), 'A');
-
-    printf("message size %lu\n", msg.size());
-
-    if (argc >= 3) {    //thread num
-        server.setThreadNum(atoi(argv[2]));
-    }
-
-    server.start();
     server.setConnectionCallback(onConnection);
     server.setMessageCallback(onMessage);
 
-    loop.runEvery(g_interval, calThroughPut);
+    if (threadCount > 1)
+    {
+      server.setThreadNum(threadCount);
+    }
+
+    server.start();
 
     loop.loop();
-    return 0;
+  }
 }
+
